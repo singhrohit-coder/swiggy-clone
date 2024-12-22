7fd79947fb5eb86b09656a205cd5681daca6f578
singhrohit-coder <iknowrohit19@gmail.com>
2024-12-23 00:17:08 +0530

    React life Cycle Completed

--- src/components/About.js
+++ src/components/About.js
@@ -6,22 +6,20 @@ class About extends Component {
     constructor(props) {
         super(props);
 
-        console.log("Parent Constructor");
+        //console.log("Parent Constructor");
     }
 
     componentDidMount () {
-        console.log("Parent Component Did Mount")
+        //console.log("Parent Component Did Mount")
     }
     
     render() {
-        console.log("Parent Render")
+        //console.log("Parent Render")
         return (
             <div>
                 <h1>About Class Based Component</h1>
                 <h2>This is Namaste React Series</h2>
-                <UserClass name={"First (classes)"} location={"Chandigarh (Classes)"} contact={"iknowrohit19@gmail.com (Classes)"} />
-                {/* i can use different instance of same class but i can passs different props over here  */}
-                <UserClass name={"Second (classes)"} location={"Barely (Classes)"} contact={"iknowabhishek19@gmail.com (Classes)"} />
+                <UserClass name={"First"} location={"Chandigarh"} contact={"iknowrohit19@gmail.com"} />
             </div>
         );
     }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,41 +7,50 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
 
-        console.log(props);
+        //console.log(props);
         console.log(this.props.name + "Child Constructor");
 
-        this.state = {  // state variables -> a big object which contains all the state variables.
-            count: 0,
-            count2: 1,  // create multiple state variable in the same state bcz this.state variable contains all the state variables. 
+        this.state = {
+            userInfo: {
+                name: "Dummy",
+                locaton: "Default",
+                contact: "Default"
+            }
         };
     }
-    componentDidMount () {
-        console.log(this.props.name + "Child Component Did Mount")
+    async componentDidMount () {
+        //console.log(this.props.name + "FirstChild Component Did Mount")
         // For API Call
+        const data = await fetch ("https://api.github.com/users/singhrohit-coder");
+        const json = await data.json(); // not writing await leading to [promise] -> [pending]
+
+        // updating our state variable / json data.
+        this.setState ({
+            userInfo: json,
+        });
+        console.log(json);
     }
 
+    componentDidUpdate() {
+        console.log("Component Did Update");
+    }
+
+    // This method is called just before the component is removed from the DOM
+    componentWillUnmount() {
+        console.log("Component Will Unmount");
+    }
+ 
     render() {
-        console.log(this.props.name + "Child Render")
-        const {name, location, contact} = this.props
-        const {count, count2} = this.state
+        //const { name, location, contact} = this.props;
+        console.log(this.props.name + "FirstChild Render")
+
+        const { name, location, contact } = this.state.userInfo;
+       
         return (
             <div className="user-card">
-                <h1>Count: {count}</h1>
-                {/* <h1>Count2: {count2}</h1> */}
-                <button 
-                onClick={() => {
-                    // Never update state Directly
-                    this.setState({
-                        count: this.state.count + 1,
-                        // count2: this.state.count2 + 1,
-                    });
-                }}
-                >
-                    Count Increase
-                </button>
                 <h2>Name: {name}</h2>
-                <h3>Location: {location}</h3>
-                <h4>Contact: {contact}</h4>
+                <h2>Location: {location}</h2>
+                <h2>Contact: {contact}</h2>
             </div>
         );
     }
