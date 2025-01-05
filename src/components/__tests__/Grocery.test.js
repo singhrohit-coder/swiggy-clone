810cb27f75218dec04113a4363e7e2a52ecaa7a0
singhrohit-coder <iknowrohit19@gmail.com>
2025-01-06 02:13:22 +0530

    unit testing completed

--- src/components/__tests__/Grocery.test.js
+++ src/components/__tests__/Grocery.test.js
@@ -9,3 +9,23 @@ test("should load page successfully", () => {
     
     expect(heading).toBeInTheDocument();
 });
+
+// test("should load buton successfully", () => {
+//     render(<Grocery />);
+
+//     // there are diffferent roles in our html and these roles are defined by jest in the testing library
+//     const button = screen.getByRole("button");
+    
+//     expect(button).toBeInTheDocument();
+// });
+
+it("should load h1Element successfully", () => {
+    render(<Grocery />);
+
+    const h1Element = screen.getByText("Our Grocery Store & we have a lot of child components inside this web page!!");
+
+    expect(h1Element).toBeInTheDocument();
+});
+
+// Note = Whenever there are multiple items we write All.
+// test/it both works it depends on person to person.
---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,43 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Header from "../Header";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom"
+
+it ("should load Header component with a login button", () => {
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header />{/* // shows error couldn't understand the react-redux component. // For that add Provider. */}
+    </Provider>
+    {/*// After that => The above error occurred in the <Link> component.
+    // This <Link/> is coming from react-router dom.
+    // For that give Browser Router. Now it will understand <Link>. */}
+    </BrowserRouter>
+    );
+
+    // let's check does it have a login button or not.
+    const loginButton = screen.getByRole("button"); // -> for specific add extra parameters , ({ name: "Sign in" });
+    // const loginButton = screen.getByText("Login"); -> not the good way
+
+    expect(loginButton).toBeInTheDocument();
+});
+
+it ("should change Sign in button to Logout on click", () => {
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header />
+    </Provider>
+    </BrowserRouter>
+    );
+
+    const signinButton = screen.getByRole("button");
+
+    fireEvent.click(signinButton);
+
+    const logoutButton = screen.getByRole("button");
+
+    expect(signinButton).toBeInTheDocument();
+});
