2b60a2be9a509d5baf1c5d389949c0cf19fa08d2
singhrohit-coder <iknowrohit19@gmail.com>
2025-02-02 21:58:36 +0530

    add social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import { Link } from "react-router-dom";
+import { VscGithubInverted } from "react-icons/vsc"
+import { CgMail } from "react-icons/cg";
+import { FaLinkedinIn } from "react-icons/fa";
+import About from "./About";
+
 
 export const Footer = () => {
 
@@ -7,19 +12,27 @@ export const Footer = () => {
     return (
 
         // footer shadow-gray-400 mt-5
-        <div className="flex bg-gray-200 justify-center shadow-md">
+        <div className="flex bg-orange-400 justify-center shadow-md">
             <div className="foot-items">
                 <ul className="flex  p-4 m-4">
                     
-                    <li className="px-4 xNIjm">Help</li>
-                    <li className="px-4 xNIjm">
-                        <Link to="/about">About us</Link>
+                    <li className="px-2 font-semibold text-white"><Link to="/about">About</Link></li>
+                    <h1 className="px-4 font-semibold text-white">Created By Rohit Singh</h1>
+                    <li className="px-2 xNIjm">
+                        <Link to="https://mail.google.com/mail/u/0/?tab=rm&ogbl#all?compose=new">
+                        <CgMail className="size-6 text-white"/>
+                        </Link>
                     </li>
-                    <li className="px-4 xNIjm">
-                        <Link to="/contact">Contact us</Link>
+                    <li className="px-2">
+                        <Link to="https://github.com/singhrohit-coder">
+                        <VscGithubInverted className="size-6 text-white"/>
+                        </Link>
                     </li>
-                    <li className="px-4 xNIjm">Legal</li>
-                    <li className="px-4 xNIjm">Social links</li>
+                    <li className="px-2 xNIjm">
+                        <Link to="linkedin.com/in/rohitsingh1912">
+                        <FaLinkedinIn className="size-6 text-white"/>
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </div>
