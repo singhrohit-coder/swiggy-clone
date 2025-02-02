b7d78e8f8efa061d816ccdf8d810939bba777b9a
singhrohit-coder <iknowrohit19@gmail.com>
2025-02-02 21:44:48 +0530

    filter button

--- src/components/RateButton.js
+++ src/components/RateButton.js
@@ -6,7 +6,7 @@ const RateButton = ({ listOfRestaurants, setFilteredRestaurants}) => {
             <div className="filter m-2 p-4 flex items-center">
         <button
         // filter-btn
-          className="px-4 py-1 font-medium bg-white border border-gray-500 mx-2 rounded-full"
+          className="px-4 py-1 font-medium bg-gray-200  mx-2 rounded-md"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4.2
