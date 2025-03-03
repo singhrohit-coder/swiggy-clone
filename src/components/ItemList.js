506e16970e193f4a67b832e70210c90f1a2edfab
singhrohit-coder <iknowrohit19@gmail.com>
2025-03-03 20:11:46 +0530

    when click on add button inc and dec button appear

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,7 @@
 import { RESTAURANT_CARD_IMAGE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addItem, removeItem } from "../utils/cartSlice";
+import { addItem, incrementCount, decrementCount } from "../utils/cartSlice";
+import { MdStars } from "react-icons/md";
 
 const ItemList = ({items}) => {
     //console.log(items);
@@ -12,12 +13,27 @@ const ItemList = ({items}) => {
     const handleAddItems = (item) => {
         dispatch(addItem(item));
     };
-    const handleRemoveItems = (item) => {
-        dispatch(removeItem(item));
+    // const handleRemoveItems = (item) => {
+    //     dispatch(removeItem(item));
+    // };
+
+    const handleIncrement = (id) => {
+        dispatch(incrementCount(id));
     };
 
-    const isItemInCart = (item) => {
-        return cartItems.some((cartItem) => cartItem?.card?.info?.id === item?.card?.info?.id);
+    const handleDecrement = (id) => {
+        dispatch(decrementCount(id));
+    };
+
+    const itemQuantity = (id) => {
+        const item = cartItems.find((cartItem) => cartItem.id === id);
+
+        return item ? item.quantity : 0;
+    };
+
+    const isItemInCart = (id) => {
+        // Check if any item in the cart matches the given item by comparing their IDs
+        return cartItems.some((cartItem) => cartItem?.card?.info?.id === id);
     };
 
     return (
@@ -26,37 +42,50 @@ const ItemList = ({items}) => {
                 <div
                 data-testId = "foodItem" // for testing.. 
                 key = {item?.card?.info?.id} 
-                className="p-2 m-2 border-yellow-100 border-b-2 flex justify-between">
+                className="cursor-pointer p-2 m-2 border rounded-lg shadow-md flex justify-between">
                     <div className="w-9/12">
                     <div className="py-2">
-                        <div className="text-lg font-bold">{item?.card?.info?.name}</div>
-                        <div className="text-lg font-semibold"> ₹ {item?.card?.info?.price 
+                        <div className="text-lg text-left font-bold">{item?.card?.info?.name}</div>
+                        <div className="text-lg text-left font-semibold"> ₹ {item?.card?.info?.price 
                         ? item?.card?.info?.price / 100
                         : item?.card?.info?.defaultPrice / 100}
                         </div>
                         
-                        <div className="text-lg font-semibold">⭐ {item?.card?.info?.ratings?.aggregatedRating?.rating}</div>
+                        <div className="text-lg text-left font-semibold flex">
+                        <MdStars className="text-green-600 mr-1 size-5 mt-1.5" />
+                            <span >{item?.card?.info?.ratings?.aggregatedRating?.rating}</span>
+                        </div>
                     </div>
-                    <p className="text-sm font-medium">{item?.card?.info?.description}</p>
+                    <p className="text-sm text-left font-medium">{item?.card?.info?.description}</p>
                     </div>
                     <div className="w-3/12 p-2">
                     <div className="relative">
                       <img
                       src={RESTAURANT_CARD_IMAGE_URL + item?.card?.info?.imageId}
-                      className="w-full rounded-lg"
+                      className="w-full rounded-lg border border-gray-300"
                       alt="Dish"
                       />
                       <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
                       {/* Button Container */}
                       <div className="bg-white rounded-lg shadow-lg border border-gray-300 px-8 py-1">
-                        {isItemInCart(item) ? (
-                            <button
-                            className="text-green-600 font-bold text-lg"
-                            onClick={() => handleRemoveItems(item)}
-                            >
-                              REMOVE
-                              </button> 
+                        {/* check if the item is alread in the cart */}
+                        {isItemInCart(item?.card?.info?.id) ? (
+                                        <div>
+                                            <button
+                                                className="text-green-600 font-bold text-lg"
+                                                onClick={() => handleIncrement(item?.card?.info?.id)} // Pass item id
+                                            >
+                                                +
+                                            </button>
+                                            <button
+                                                className="text-green-600 font-bold text-lg"
+                                                onClick={() => handleDecrement(item?.card?.info?.id)} // Pass item id
+                                            >
+                                                -
+                                            </button>
+                                        </div> 
                         ) : (
+                            // if item is not in the cart show add button
                             <button
                         className="text-green-600 font-bold text-lg"
                         onClick={() => handleAddItems(item)}
