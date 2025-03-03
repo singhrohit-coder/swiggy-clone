4bb934a6b3f7a4021de154e1b655045dce4b3354
singhrohit-coder <iknowrohit19@gmail.com>
2025-03-03 19:24:23 +0530

    adding down icon

--- src/components/RestaurantCategories.js
+++ src/components/RestaurantCategories.js
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
+import { CiCircleChevDown, CiCircleChevUp } from "react-icons/ci";
 
 const RestaurantCategories = ({ data }) => {
-    const [showItems, setShowItems] = useState(false);
+    const [showItems, setShowItems] = useState(true);
 
-    const handleClick = () => {
+    const openClick = () => {
         setShowItems(!showItems);
         // setShowCarousel(!showCarousel);
     };
     
     return (
-        <div className="py-4">
+        <div className="mt-2 py-1">
+        <div className="rounded-xl  bg-zinc-100 py-2 shadow-xl">
             {/* Header */}
-            <div className="rounded-lg bg-orange-300 shadow-md py-2 px-2">
-            <div className="flex justify-between cursor-pointer"  onClick={handleClick}>
-                <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
-                <span>🔽</span>
+            <div>
+            <div className=" cursor-pointer flex justify-between items-end pr-6"  
+            onClick={openClick}>
+                <span className=" pl-3 font-semibold text-lg">{data.title} ({data.itemCards.length})    
+                </span>
+                <span className="text-2xl">
+                {showItems ? <CiCircleChevUp /> : <CiCircleChevDown />}</span>
             </div>
             {/* Accordion Body */}
             {showItems && <ItemList items = {data.itemCards} />}
             </div>
+        </div>
         </div>
     );
 };
